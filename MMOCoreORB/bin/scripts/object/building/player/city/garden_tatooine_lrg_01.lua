573ac4b6fc20d757bd2846ab2d4c7ee6d3cda5d1
RelicsMnemon <blygilmore@gmail.com>
2020-04-03 21:08:26 -0400

    Gardens Buildable on all planets

--- MMOCoreORB/bin/scripts/object/building/player/city/garden_tatooine_lrg_01.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/garden_tatooine_lrg_01.lua
@@ -45,7 +45,7 @@ object_building_player_city_garden_tatooine_lrg_01 = object_building_player_city
 
 	lotSize = 0,
 	baseMaintenanceRate = 0,
-	allowedZones = {"dantooine", "lok", "tatooine"},
+	allowedZones = {"corellia", "dantooine", "lok", "naboo", "rori", "talus", "tatooine"},
 	length = 7,
 	width = 7,
 	cityRankRequired = 3,
