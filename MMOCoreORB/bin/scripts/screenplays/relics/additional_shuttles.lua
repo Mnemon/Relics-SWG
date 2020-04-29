bea599029b887cc5ea177c52a303ba7833922c7f
RelicsMnemon <blygilmore@gmail.com>
2020-04-29 15:56:05 -0400

    Aurilian Outpost Shuttle Added, Cleaned up Naming of Shuttles & Starports, Players Now Spawn at Starports Similar to Theed

--- MMOCoreORB/bin/scripts/screenplays/relics/additional_shuttles.lua
+++ MMOCoreORB/bin/scripts/screenplays/relics/additional_shuttles.lua
@@ -18,15 +18,14 @@ function AdditionalShuttlesPlay:spawnSceneObjects()
 	spawnSceneObject("tatooine", "object/building/tatooine/shuttleport_tatooine.iff", 3775, 7, 2266, 0, 1, 0, 0, 0) -- Mos Taike
 	spawnSceneObject("tatooine", "object/building/tatooine/shuttleport_tatooine.iff", -5051, 75, -6548, 0, 1, 0, 0, 0) -- Wayfar
 	spawnSceneObject("lok", "object/building/military/outpost_starport.iff", -1747, 12, -3033, 0, 1, 0, 0, 0) -- Imp Outpost Lok
+	spawnSceneObject("dathomir", "object/building/military/outpost_starport.iff", 4632, 123, -3228, 0, 1, 0, 0, 0) -- Aurillian Outpost
 	
 	
 end
 
 function AdditionalShuttlesPlay:spawnMobiles()
 
-	--Theater NPCs
-	  -- spawnMobile("lok", "junk_dealer", 60, -284.0, 12.0, -3019.8, -1, 0) -- NF Outpost
-	  -- spawnMobile("lok", "informant_npc_lvl_3", 60, -299.0, 12.0, -3019.8, -1, 0) -- NF Outpost
-	  
-	  
+	--Imp Outspot NPCs
+	  spawnMobile("lok", "stormtrooper", 300, -1762.81, 12.0, -3052.32, -45, 0)
+	  spawnMobile("lok", "stormtrooper", 300, -1762.81, 12.06, -3048.58, -45, 0)
 end
