5945c1d7161c77a500f2c36b95929ec7459fb6c8
RelicsMnemon <blygilmore@gmail.com>
2020-04-29 17:11:44 -0400

    Blue Frog Buffs decreased to 1500/500 for 1.5 hours

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -48,10 +48,10 @@ onlineCharactersPerAccount = 4 --How many characters are allowed online from a s
 allowSameAccountPvpRatingCredit = 0
 
 --Blue frog / GM buff values
-performanceBuff = 1000
-medicalBuff = 1750
-performanceDuration = 7200 -- in seconds
-medicalDuration = 7200 -- in seconds
+performanceBuff = 500
+medicalBuff = 1500
+performanceDuration = 5400 -- in seconds
+medicalDuration = 5400 -- in seconds
 
 --Sets the experience multiplier while grouped
 groupExpMultiplier = 1.5
