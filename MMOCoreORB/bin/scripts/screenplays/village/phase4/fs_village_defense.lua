9587e3d649d0486d6f3aa5058319a44e86bcb603
RelicsMnemon <blygilmore@gmail.com>
2020-05-07 13:07:09 -0400

    Village Healing Quest Reduced to 2

--- MMOCoreORB/bin/scripts/screenplays/village/phase4/fs_village_defense.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/phase4/fs_village_defense.lua
@@ -71,10 +71,10 @@ function FsVillageDefense:checkHealerList(pCreature)
 			local pQuest
 
 			if (QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_COMBAT_HEALING_2)) then
-				healReq = 100
+				healReq = 2
 				pQuest = getQuestInfo("fs_combat_healing_2")
 			else
-				healReq = 50
+				healReq = 2
 				pQuest = getQuestInfo("fs_combat_healing_1")
 			end
 
